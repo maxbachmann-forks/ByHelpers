ff2d17f4ce5a6b742c5b54f803ceb9414365ba01
Krakkev <kevangy@hotmail.com>
2018-09-24 12:25:24 -0500

    other setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name = 'ByHelpers',
-    packages = setuptools.find_packages(), # this must be the same as the name above
+    packages = find_namespace_packages(), # this must be the same as the name above
     version = '0.0.0',
     description = 'Library used by ByPrice scrapers',
     author = 'Kevin B. Garcia Alonso',
