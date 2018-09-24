467f9c4a42bb22309b1258e4713e9d639bba1014
Krakkev <kevangy@hotmail.com>
2018-09-24 12:48:00 -0500

    Fixing library setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
-from setuptools import find_namespace_packages
+from setuptools import find_packages
 
 
 setuptools.setup(
     name = 'ByHelpers',
-    packages = find_namespace_packages(), # this must be the same as the name above
+    packages = ['ByHelpers'], # this must be the same as the name above
     version = '0.0.0',
     description = 'Library used by ByPrice scrapers',
     author = 'Kevin B. Garcia Alonso',
