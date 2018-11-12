7aa307d2db6189c24b5588ea33d2b5fab3abb5e2
Krakkev <kevangy@hotmail.com>
2018-11-12 12:00:42 -0600

    Setup new version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import find_packages
 setuptools.setup(
     name = 'ByHelpers',
     packages = ['ByHelpers'], # this must be the same as the name above
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'Library used by ByPrice scrapers',
     author = 'Kevin B. Garcia Alonso',
     author_email = 'kevangy@hotmail.com',
     url = 'https://github.com/ByPrice/ByHelpers', # use the URL to the github repo
-    download_url = 'https://github.com/ByPrice/ByHelpers/tarball/0.0.3',
+    download_url = 'https://github.com/ByPrice/ByHelpers/tarball/0.0.4',
     keywords = ['ByPrice'],
     install_requires=[
         'pika==0.10.0',
