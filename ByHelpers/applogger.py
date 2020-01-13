e832a8dda495e9663b22b0aafdbda2a4a423898e
yogesh <yogesh@byprice.com>
2020-01-13 14:29:23 -0600

    app loger with td-agent and new config

--- ByHelpers/applogger.py
+++ ByHelpers/applogger.py
@@ -1,143 +1,124 @@
-#-*- coding: utf-8-*-
-import socket
 import logging
-from logging.handlers import SysLogHandler
-import google.cloud.logging
+from logging import config as logging_config
 import os
 
 logger = None
-APP_NAME = os.getenv('APP_NAME', '__name__')
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
-LOG_HOST = os.getenv('LOG_HOST', 'logs5.papertrailapp.com')
-LOG_PORT = os.getenv('LOG_PORT', 27971)
-ENV = os.getenv('ENV', 'LOCAL')
+APP_NAME = os.getenv('APP_NAME', 'applogger')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+ENVIRONMENT = os.getenv('ENV', 'LOCAL')
+REGION = os.getenv('REGION', 'MEX')
+ACTIVE_LOG_HANDLERS = os.getenv('ACTIVE_LOG_HANDLERS',
+                                ['console', 'info_file_handler', 'error_file_handler', 'fluent_async_handler'])
 
-# Instantiates a client
-client = None
-try:
-    client = google.cloud.logging.Client()
-    # # Connects the logger to the root logging handler with log level debug or higher
-    # client.setup_logging(log_level=getattr(logging,LOG_LEVEL))
-except Exception:
-    logging.info('Not GCP credentials')
+app_logging_config = {
+    'version': 1,
+    'loggers': {
+        '': {  # root logger
+            'level': 'NOTSET',
+            'handlers': ['console', 'info_file_handler', 'error_file_handler'],
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'formatter': 'info',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'info_file_handler': {
+            'level': 'INFO',
+            'formatter': 'info',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '../logs/application_name_info.log',
+            'mode': 'a',
+            'maxBytes': 1048576,
+            'backupCount': 10
+        },
+        'error_file_handler': {
+            'level': 'WARNING',
+            'formatter': 'error',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '../logs/application_name_error.log',
+            'mode': 'a',
+            'maxBytes': 1048576,
+            'backupCount': 10
+        },
+        'fluent_async_handler': {
+            'level': 'INFO',
+            'class': 'fluent.handler.FluentHandler',
+            'host': '0.0.0.0',
+            'port': 24224,
+            'tag': 'app.dynamic_app_tag',
+            'buffer_overflow_handler': 'overflow_handler',
+            # 'queue_circular': True,
+            'formatter': 'night_watch'
+
+        }
+    },
+    'formatters': {
+        'info': {
+            'format': '%(asctime)s-%(module)s-%(lineno)s::%(levelname)s:: %(message)s'
+        },
+        'error': {
+            'format': '%(asctime)s-%(module)s-%(lineno)s::%(levelname)s:: %(message)s'
+        },
+        'night_watch': {
+            '()': 'fluent.handler.FluentRecordFormatter',
+            'format': {
+                'time': '%(asctime)s',
+                'level': '%(levelname)s',
+                'hostname': '%(hostname)s',
+                'where': '%(module)s-%(lineno)s',
+            }
+        }
+    },
+
+}
 
 
 def create_logger(name=APP_NAME):
-    ''' Create logger and add handlers and filters
-    '''
     global logger
+
+    log_level = LOG_LEVEL
+    app_logging_config['loggers']['']['handlers'] = list(ACTIVE_LOG_HANDLERS)
+
+    if log_level is not None and log_level in ['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+        print('Changing LOG_LEVEL={} for console, info_file_handler and fluent_async_handler'.format(log_level))
+        app_logging_config['handlers']['console']['level'] = log_level
+        app_logging_config['handlers']['info_file_handler']['level'] = log_level
+        app_logging_config['handlers']['fluent_async_handler']['level'] = log_level
+
+    log_info_file = os.environ.get('LOG_INFO_FILE')
+    if log_info_file is None or log_info_file == '':
+        log_info_file = os.path.join(os.getcwd(), 'logs', APP_NAME + '_info.log')
+        print('LOG_INFO_FILE={}'.format(log_info_file))
+    app_logging_config['handlers']['info_file_handler']['filename'] = log_info_file
+
+    log_error_file = os.environ.get('LOG_ERROR_FILE')
+    if log_error_file is None or log_error_file == '':
+        log_error_file = os.path.join(os.getcwd(), 'logs', APP_NAME + '_error.log')
+        print('ERROR_INFO_FILE={}'.format(log_error_file))
+    app_logging_config['handlers']['error_file_handler']['filename'] = log_error_file
+
+    app_logging_config['handlers']['fluent_async_handler']['tag'] = APP_NAME + '.' + ENVIRONMENT + '.' + REGION
+
+    app_logging_config['formatters']['night_watch']['format']['hostname'] = \
+        app_logging_config['handlers']['fluent_async_handler']['tag']
+
+    logging_config.dictConfig(app_logging_config)
     logger = logging.getLogger(name)
-
-    log_format = '%(service)s %(hostname)s: [%(env)s] %(message)s '
-    log_formatter = logging.Formatter(log_format,datefmt='%b %d %H:%M:%S')
-    log_filter = ContextFilter()
-    remote_handler = SysLogHandler(address=(LOG_HOST, int(LOG_PORT)))
-    remote_handler.setFormatter(log_formatter)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-
-    if client is not None:
-        hdlr = client.get_default_handler()
-        # google.cloud.logging.handlers.setup_logging(hdlr)
-        logger.addHandler(hdlr)
-        logger.addHandler(logging.StreamHandler())
-    
-    logger.setLevel(LOG_LEVEL)
-    logger.addFilter(log_filter)
-
-    
-    logger.addHandler(console_handler)
-    logger.addHandler(remote_handler)
+    logger.debug(app_logging_config)
 
 
 def get_logger():
-    ''' Get the app logger by the app name
-    '''
     return logging.getLogger(APP_NAME)
 
 
-class ContextFilter(logging.Filter):
-    hostname = socket.gethostname()
-    service = APP_NAME
-    env = ENV
-
-    def filter(self, record):
-        record.hostname = ContextFilter.hostname
-        record.service = ContextFilter.service
-        record.env = ContextFilter.env
-        return True
-
-
-class AppLogger(object):
-    ''' General apps logger
-    '''
-    logger = None
-    level = None
-    format = '%(asctime)s %(service)s %(hostname)s %(levelname)s: %(message)s'
-    filer = None
-    # handlers
-    console = None
-    remote = None
-    # remote 
-    host = None
-    port = None
-    
-    def __init__(self, level=LOG_LEVEL, host=LOG_HOST, port=LOG_PORT):
-        ''' Instantiate loggers (remote and local)
-            - Console handler: log to console
-            - PaperTrail handler: send logs to remote host
-        '''
-        self.level = getattr(logging,level)
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(self.level)
-        self.formatter = logging.Formatter(self.format,datefmt='%b %d %H:%M:%S')
-        self.filter = ContextFilter()
-
-        # Console Handler
-        self.console = logging.StreamHandler()
-        self.console.setFormatter(self.formatter)
-        self.console.addFilter(self.filter)
-        self.logger.addHandler(self.console)
-
-        # Remote handler
-        if host != None and port != None:
-            self.host = host
-            self.port = int(port)
-            self.remote = SysLogHandler(address=(self.host, self.port))
-            self.remote.addFilter(self.filter)
-            self.remote.setFormatter(self.formatter)
-            self.logger.addHandler(self.remote)
-
-    def info(self,msg):
-        ''' Info logging
-        '''
-        self.logger.info(msg)
-
-    def debug(self,msg):
-        ''' Debug logging
-        '''
-        self.logger.debug(msg)
-
-    def warn(self,msg):
-        ''' Warning logging
-        '''
-        self.logger.warn(msg)
-
-    def error(self,msg):
-        ''' Error logging
-        '''
-        self.logger.error(msg)
-
-    def critical(self,msg):
-        ''' Critical logging
-        '''
-        self.logger.critical(msg)
-
-
 if __name__ == '__main__':
+    create_logger(APP_NAME)
     logger = get_logger()
-    logger.info('Info message sent!!')
-    logger.debug('Debug message sent....')
-    logger.warning('Warning message sent....')
-    logger.error('Error!!! message sent....')
+    logger.debug('Debug message')
+    logger.info('Info message')
+    logger.warning('Warning message sent')
+    logger.error('Error message sent')
+    logger.critical('Critical message sent')
